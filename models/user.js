68f1ac7a05efee0e7966f4a3eb41c05e37e2f1b2
Kimberly West <kdenton993@gmail.com>
2022-06-26 13:46:24 -0600

    added users models

---
+++ models/user.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    trim: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: [/.+@.+\..+/],
+  },
+});
+
+const User = mongoose.model("User", UserSchema);
+
+model.exports = User;
