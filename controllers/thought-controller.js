34979a1e73344526e140febe6378f8c66c39cd05
Kimberly West <kdenton993@gmail.com>
2022-07-05 18:28:11 -0600

    setting up update and delete thoughts in thought controller

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -49,3 +49,74 @@ createThought({body}, res) {
     })
     .catch(err => res.status(400).json(err));
 },
+
+updateThought({ params, body}, res) {
+    Thought.findOneAndUpdate (
+        {_id:params.id },
+        body,
+        { new: true }
+    )
+    .then(dbThoughtData =>  {
+        if(!dbThoughtData) {
+            res.status(404).json({message: 'No thought associated with this id!'});
+            return;
+        }
+        res.json(dbThoughtData);
+    })
+    .catch(err => res.status(400).json(err));
+},
+
+deleteThought({ params }, res) {
+    
+    Thought.findOneAndDelete({ _id: params.id })
+    .then(dbThoughtData => {
+        if (!dbThoughtData) {
+            res.status(404).json({ message: 'No thought found with this id'});
+            return;
+        }
+       
+        User.findOneAndUpdate(
+            { username: dbThoughtData.username },
+            { $pull: { thoughts: params.id } }
+        )
+        .then(() => {
+            res.json({message: 'Thought Deleted'});
+        })
+        .catch(err => res.status(500).json(err));
+    })
+    .catch(err => res.status(500).json(err));
+},
+
+addReaction({ params, body}, res) {
+    Thought.findOneAndUpdate (
+        {_id: params.thoughtId}, 
+        {$addToSet: {reactions:body}},
+        {new: true, runValidators: true}
+    )
+    .then(dbThoughtData => {
+        if (!dbThoughtData) {
+            res.status(404).json({message: 'No thought associated wih this id'});
+            return;
+        }
+        res.json(dbThoughtData);
+    })
+    .catch(err => res.status(500).json)(err);
+}
+
+deleteReaction({params, body}, res); {
+    Thought.findOneAndUpdate(
+        {_id: params.thoughtId},
+        {$pull: {reactions:{ reactionId: body.reactionId}}},
+        {new: true, runValidators: true}
+    )
+    .then(dbThoughtData = > {
+        if (!dbThoughtData) {
+            res.status(404).json({message: "No Thought associated with this id!"})
+            return;
+        }
+        res.json({message: "Reaction Deleted!"})
+    })
+    .catch(err => res.status(500).json(err));
+}
+
+module.exports = thoughtController;
