39635f81ed32ae83e7e9a7aa0641bf7831ea8b04
Kimberly West <kdenton993@gmail.com>
2022-07-06 18:11:50 -0600

    added routes for thoughts.

--- routes/api/api/thought-routes.js
+++ routes/api/api/thought-routes.js
@@ -1,5 +1,27 @@
 const router = require('express').Router();
 const {
     getAllThoughts,
-    g
-}
+    getThoughtById,
+    createThought,
+    updateThought,
+    deleteThought,
+    addReaction,
+    deleteReaction,
+} = require('../..controllers/thought-controller');
+
+router
+    .route('/')
+    .get(getAllThoughts)
+    .post(createThought);
+
+router
+    .route('/:id')
+    .get(getThoughtById)
+    .put(updateThought)
+    .delete(deleteThought);
+
+router.route('/:thoughtId/reaction')
+    .post(addReaction)
+    .delete(deleteReaction);
+
+module.exports = router;
